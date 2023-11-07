3c42037d0230444a98d9ce9450653dbb45530f7b
hannah-no <21425@apps.eggs.school.nz>
2023-11-07 14:02:18 +1300

    Create 00_mmf_base.py

---
+++ 00_mmf_base.py
@@ -0,0 +1,29 @@
+# import statements
+
+
+# functions go here
+
+
+# ************************ Main Routine ************************
+
+# Set up dictionaries / lists needed to hold data
+
+# Ask user if they have used the program before & show instructions if necessary
+
+# Loop to get tickets details
+
+    # Get name (can't be blank)
+
+    # Get age (between 12 and 30)
+
+    # Calculate ticket price
+
+    # Loop to ask for snack
+
+    # Calculate snack price
+
+    # Ask for payment method (and apply surcharge if necessary)
+
+# Calculate  total sales and profit
+
+# Output data to textfile
